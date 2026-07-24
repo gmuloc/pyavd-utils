2d72431b647df266a9be320bb5758e59e13aa5f8
gmuloc <gmulocher@arista.com>
2026-07-24 14:42:14 +0200

    refactor: Move Schema Store Tests Out Of Validation Suite

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,12 +12,12 @@ from pyavd_utils.schema_store import init_store_from_file
 ADV_SCHEMA_URL = "https://github.com/aristanetworks/avd/releases/download/v6.0.0-dev3/schemas.json.gz"
 
 
-@pytest.fixture(scope="package")
-def init_store() -> None:
+@pytest.fixture(scope="session")
+def init_store(tmp_path_factory: pytest.TempPathFactory) -> None:
     from urllib.request import urlretrieve
 
     filename = Path(ADV_SCHEMA_URL).name
-    tmp_file = Path(__file__).parent.joinpath("tmp", filename)
+    tmp_file = tmp_path_factory.mktemp("schema_store") / filename
     urlretrieve(ADV_SCHEMA_URL, tmp_file)  # noqa: S310
 
     init_store_from_file(tmp_file)
